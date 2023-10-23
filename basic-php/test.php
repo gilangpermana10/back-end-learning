286008f659a7873d8b4ad7e7762eff1dc0271f66
gilang_permana <gilanghyper27@gmail.com>
2023-10-23 15:19:47 +0700

    add test files

--- basic-php/test.php
+++ basic-php/test.php
@@ -1,11 +1,10 @@
 <?php
 $bbm_harga = array(
     "pertamax" => 12500,
-    "pertalite"=> 10000,
-    "dexlite"=> 13000,
-    "solar"=> 6000
+    "pertalite" => 10000,
+    "dexlite" => 13000,
+    "solar" => 6000
 );
-
 
 echo "\n";
 echo "daftar nama Nama BBM dan harga :\n";
@@ -15,27 +14,23 @@ foreach ($bbm_harga as $key => $value) {
     $nomor_urut++;
 }
 
-   
-$jenis_bbm = readline("silahkan pilih salah satu, contoh \"1\" : \n");
-    if($jenis_bbm < 0 ) {
-        echo "Jumlah beli harus lebih dari 0 \n ";
-        exit(1);
-    }
+$jenis_bbm = intval(readline("silahkan pilih salah satu, contoh \"1\" : \n"));
+if ($jenis_bbm < 1 || $jenis_bbm > count($bbm_harga)) {
+    echo "Jenis BBM tidak valid\n";
+    exit(1);
+}
 
-    $jumlah_beli = readline("silahkan masukan jumlah BBM yang akan dibeli, contoh \"1.5\" : \n");
-    if($jumlah_beli < 0){
-        echo "Jumlah beli harus lebih dari 0 \n ";
-        exit(1);
-    }
+$jumlah_beli = readline("silahkan masukan jumlah BBM yang akan dibeli, contoh \"1.5\" : \n");
+if ($jumlah_beli < 0) {
+    echo "Jumlah beli harus lebih dari 0 \n ";
+    exit(1);
+}
 
-    $uang_bayar= readline("silahkan masukan jumlah uang yang akan dibayar, contoh \"200000\" : \n");
-    if($uang_bayar <= 0) {
-        echo "jumlah beli harus lebih dari 0 \n";
-        exit(1);
-    }
-   
-
-echo var_dump($jenis_bbm[1]);
+$uang_bayar = readline("silahkan masukan jumlah uang yang akan dibayar, contoh \"200000\" : \n");
+if ($uang_bayar <= 0) {
+    echo "Jumlah beli harus lebih dari 0 \n";
+    exit(1);
+}
 
 // Hitung total harga
 $harga_per_liter = $bbm_harga[array_keys($bbm_harga)[$jenis_bbm - 1]];
@@ -46,7 +41,7 @@ $kembalian = $uang_bayar - $total_harga;
 
 // Tampilkan output
 echo "\nRincian Pembelian:\n";
-echo "Jenis BBM: $jenis_bbm\n";
+echo "Jenis BBM: " . array_keys($bbm_harga)[$jenis_bbm - 1] . "\n";
 echo "Harga per liter: $harga_per_liter Rupiah\n";
 echo "Jumlah yang dibayar: $uang_bayar Rupiah\n";
 echo "Jumlah BBM yang dibeli: $jumlah_beli liter\n";
@@ -58,5 +53,3 @@ if ($kembalian >= 0) {
     echo "Uang yang dibayarkan kurang. Silakan bayar lebih.\n";
 }
 ?>
-
-
